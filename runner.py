062f1cce433c58da9dcb0612203ba7e7fa312665
RobertJ <r.jutresa@gmail.com>
2022-11-08 11:04:58 +0100

    Updated files

--- runner.py
+++ runner.py
@@ -1,25 +1,35 @@
+# Import everything from the function file
 from solution import *
 
-
+# Creating the dataframe to store results
 DATA_FRAME = pandas.DataFrame(columns=["Feature", "Unifrom", "Metric", "Result",
                               "Skimage", "Image Size", "R", "L", "Step", "Histogram", "Histogram Tile Size"])
 
+# Loop through image sizes and run the entire processing loop
 for size in IMAGE_SIZE:
-    IMAGES = []
+    IMAGES = []  # Define empty image array
+    # Loop through main folder
     for subfolder in os.listdir(PATH):
+        # Make and check subfolder path
         sf = os.path.join(PATH, subfolder)
         if os.path.isdir(sf):
+            # Loop through subfolder
             for image in os.listdir(sf):
+                # Make file path and check if its an image
                 image_f = os.path.join(sf, image)
                 if os.path.isfile(image_f) and "json" not in image_f:
+                    # Read image in greyscale
                     img = cv2.imread(
                         image_f, cv2.IMREAD_GRAYSCALE)
+                    # Resize the image and cast it to float to avoid overflow errors
                     img = cv2.resize(size, size).astype(np.float64)
-                    IMAGES.append(img)
+                    IMAGES.append(img)  # Add the image to the list
 
+    # Compute basic pixel by pixel feature vector
     pixel_by_pixel_features = [image.flatten()
                                for image in copy.deepcopy(IMAGES)]
 
+    # Save results of above define feature vector rank-1 classification accuracy for all three metrics
     DATA_FRAME.loc[len(DATA_FRAME.index)] = ["Pixel By Pixel", False, "euclidean", accuracy_score(
         pixel_by_pixel_features), None, size, None, None, None, None, None]
     DATA_FRAME.loc[len(DATA_FRAME.index)] = ["Pixel By Pixel", False, "cityblock", accuracy_score(
@@ -27,23 +37,28 @@ for size in IMAGE_SIZE:
     DATA_FRAME.loc[len(DATA_FRAME.index)] = ["Pixel By Pixel", False, "cosine", accuracy_score(
         pixel_by_pixel_features), None, size, None, None, None, None, None]
 
+    # Loop through radius and word length pairs
     for r, l in zip(R, L):
-        skimage_lbp = [feature.local_binary_pattern(
-            image, l, r, method="default").flatten() for image in copy.deepcopy(IMAGES)]
-        uniform_skimage_lbp = [feature.local_binary_pattern(
-            image, l, r, method="uniform").flatten() for image in copy.deepcopy(IMAGES)]
-
-        lbp_footprint = make_lbp_window(r, l)
-
-        lbp_images = [generic_filter(
-            image, simple_lbp, footprint=lbp_footprint, mode="nearest").flatten() for image in copy.deepcopy(IMAGES)]
-        uniform_lbp_images = [generic_filter(
-            image, uniform_lbp, footprint=lbp_footprint, mode="nearest").flatten() for image in copy.deepcopy(IMAGES)]
-
+        # Loop through local region overlap steps
         for step in STEP:
-            lbp_images = lbp_images[::step]
-            uniform_lbp_images = uniform_lbp_images[::step]
+            # Compute the baseline feature vectors using skimage, and remove pixels based on step value
+            skimage_lbp = [feature.local_binary_pattern(
+                image, l, r, method="default").flatten()[::step] for image in copy.deepcopy(IMAGES)]
+            uniform_skimage_lbp = [feature.local_binary_pattern(
+                image, l, r, method="uniform").flatten()[::step] for image in copy.deepcopy(IMAGES)]
 
+            # Create the relevant LBP window for computing feature vectors
+            lbp_footprint = make_lbp_window(r, l)
+
+            # Using the generic_filter function pass through the images, computing their LBP values and
+            #   removing pixels based on step value
+            lbp_images = [generic_filter(
+                image, simple_lbp, footprint=lbp_footprint, mode="nearest").flatten()[::step] for image in copy.deepcopy(IMAGES)]
+            uniform_lbp_images = [generic_filter(
+                image, uniform_lbp, footprint=lbp_footprint, mode="nearest").flatten()[::step] for image in copy.deepcopy(IMAGES)]
+
+            # Compute the rank-1 accuracy for both the implemented and skimage created feature vectors for all three metrics
+            # First normal LBP
             DATA_FRAME.loc[len(DATA_FRAME.index)] = ["LBP", False, "euclidean", accuracy_score(
                 lbp_images), accuracy_score(skimage_lbp), size, r, l, step, False, None]
             DATA_FRAME.loc[len(DATA_FRAME.index)] = ["LBP", False, "cityblock", accuracy_score(
@@ -51,6 +66,7 @@ for size in IMAGE_SIZE:
             DATA_FRAME.loc[len(DATA_FRAME.index)] = ["LBP", False, "cosine", accuracy_score(
                 lbp_images, "cosine"), accuracy_score(skimage_lbp, "cosine"), size, r, l, step, False, None]
 
+            # Then uniform LBP
             DATA_FRAME.loc[len(DATA_FRAME.index)] = ["LBP", True, "euclidean", accuracy_score(
                 uniform_lbp_images), accuracy_score(uniform_skimage_lbp), size, r, l, step, False, None]
             DATA_FRAME.loc[len(DATA_FRAME.index)] = ["LBP", True, "cityblock", accuracy_score(
@@ -58,12 +74,15 @@ for size in IMAGE_SIZE:
             DATA_FRAME.loc[len(DATA_FRAME.index)] = ["LBP", True, "cosine", accuracy_score(
                 uniform_lbp_images, "cosine"), accuracy_score(uniform_skimage_lbp, "cosine"), size, r, l, step, False, None]
 
+            # Loop through the histogram tile size values
             for hist_area in HIST_AREA:
+                # Compute histograms
                 lbp_hist = make_histograms(
                     copy.deepcopy(lbp_images), hist_area, size)
                 skimage_lbp_hist = make_histograms(
                     copy.deepcopy(skimage_lbp), hist_area, size)
 
+                # And save their rank-1 classification accuracy
                 DATA_FRAME.loc[len(DATA_FRAME.index)] = ["LBP", False, "euclidean", accuracy_score(
                     lbp_hist), accuracy_score(skimage_lbp_hist), size, r, l, step, True, hist_area]
                 DATA_FRAME.loc[len(DATA_FRAME.index)] = ["LBP", False, "cityblock", accuracy_score(
@@ -71,6 +90,7 @@ for size in IMAGE_SIZE:
                 DATA_FRAME.loc[len(DATA_FRAME.index)] = ["LBP", False, "cosine", accuracy_score(
                     lbp_hist, "cosine"), accuracy_score(skimage_lbp_hist, "cosine"), size, r, l, step, True, hist_area]
 
+                # Repeat for uniform LBP
                 uniform_lbp_hist = make_histograms(copy.deepcopy(
                     uniform_lbp_images), hist_area, size)
                 uniform_skimage_lbp_hist = make_histograms(
--- solution.py
+++ solution.py
@@ -1,14 +1,16 @@
+# Library imports used for implementing the LBP descriptor
 import os
 import numpy as np
 import cv2
 import copy
 from scipy.spatial.distance import cdist
 from scipy.ndimage import generic_filter
-# Baseline imports
+# Import for baseline testing with skimage
 from skimage import feature
-
+# Import for saving data
 import pandas
 
+# Defining global variables with different parameter values
 PATH = "./awe"
 IMAGE_SIZE = [(64, 64), (128, 128), (256, 256)]
 R = [1, 2, 3]
@@ -17,63 +19,84 @@ HIST_AREA = [16, 32, 64]
 STEP = [1, 2, 4]
 
 
-def accuracy_score(array, metric="euclidean"):
+def accuracy_score(array, metric="euclidean"):  # Accuracy score function
+    # Computing the distance matrix using cdist from scipy with specified metric
     distance_matrix = cdist(
         array, array, metric=metric)
+    # Filling up the diagonal with infinity to avoid 100% recognition
     np.fill_diagonal(distance_matrix, np.inf)
+    # Computing the truth vector or correctly predicted classes
     truth_vector = np.array(
         [i // 10 == pred // 10 for i, pred in enumerate(np.argmin(distance_matrix, 1))])
+    # Returning the ratio of correctly predicted classes to all processed images
     return np.sum(truth_vector) / truth_vector.shape[0]
 
 
+# Function to create a histogram for every image
 def make_histograms(array, area, image_size, bins=2**L):
     for index, image in enumerate(array):
+        # Calling the histogram making function for a single image
         array[index] = make_hist_for_image(image, area, image_size, bins)
     return array
 
 
+# Function to create a concatenated histogram for one image
 def make_hist_for_image(image, area, image_size, bins):
-    histogram = []
+    histogram = []  # empty array to which to add area histograms
+    # Looping through the rows and columns based on area size
     for row in range(0, image_size[0], area):
         for col in range(0, image_size[1], area):
+            # Using numpy implemented histogram function for an area
             hist, _ = np.histogram(
                 image[row:row+area, col:col+area].astype(int), bins=np.arange(bins + 1))
+            # Adding area histogram to the histogram for the whole image
             histogram.extend(hist)
     return np.array(histogram)
 
 
-def make_lbp_window(r, l):
+def make_lbp_window(r, l):  # Function to create the window for processing LBP area
+    # Computing the rotational indices of applicable pixels
     indices = [np.around(np.array(
         (r * np.sin(2*u*np.pi/l), r * np.cos(2*u*np.pi/l)))).astype(int) for u in range(l)]
-
+    # Creating a empty window of values
     window = np.zeros((1 + np.ptp(indices), 1 + np.ptp(indices)))
+    # Finding the center node
     window_center = (window.shape[0] // 2, window.shape[1] // 2)
     window[window_center] = 1
+    # Setting other nodes based on center node coordinates and computed indices
     for x in indices:
         cords = x + window_center
         window[cords[0], cords[1]] = 1
     return window
 
 
-def simple_lbp(values):
+def simple_lbp(values):  # Function to compute the simple LBP
+    # Taking and removing the center value so it doesn't process
     center = np.take(values, values.size // 2)
     values = np.delete(values, values.size // 2)
+    # Calculating the differences/binary values of neighboring pixels
     values = np.array([(value - center >= 0) * 2 **
                       index for index, value in enumerate(values)])
-    return np.sum(values)
+    return np.sum(values)  # Returning their sum as LBP feature
 
 
+# Function to compute the bitwise changes
 def uniform_calculation(values, center_value):
+    # Computing the first part of the equation
     u_1 = np.abs(int((values[-1] - center_value) >= 0) -
                  int((values[0] - center_value) >= 0))
+    # Computing the second part of the equation
     u_2 = np.sum(np.abs((values[1:] - center_value) >= 0).astype(int) -
                  ((values[:-1] - center_value) >= 0).astype(int))
+    # Returning the required truth value
     return (u_1 + u_2) <= 2
 
 
-def uniform_lbp(values):
+def uniform_lbp(values):  # Function to compute uniform LBP
+    # Taking and removing the center value so it doesn't process
     center = np.take(values, values.size // 2)
     values = np.delete(values, values.size // 2)
+    # Returning the correct value of the pixel based on uniform_calculation truth value
     if uniform_calculation(values, center):
         return np.sum((values - center) >= 0)
     return L + 1
